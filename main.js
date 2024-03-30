3880bbb6105b066cc6fa723bd7d278f2a47c237b
flebedev77 <flebedev99@gmail.com>
2024-03-30 18:01:52 +1030

    Added functionality and styled the page

--- main.js
+++ main.js
@@ -3,12 +3,13 @@ const { app, BrowserWindow } = require('electron');
 const createWindow = () => {
     const win = new BrowserWindow({
         width: 800,
+        minWidth: 600,
         height: 600,
     })
 
     win.setTitle("HCTCSSF");
     win.setMenu(null);
-    win.setIcon("ui/icons/palette.png");
+    win.setIcon("ui/icons/3d/icon_resized.png");
 
     win.loadFile('index.html')
 }
